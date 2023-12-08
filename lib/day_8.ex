6c6b92e3372560a3fdf75d2ed21512cf929b1420
Mateusz Pietrzak <mateusz.jerzy.pietrzak@gmail.com>
2023-12-08 11:51:49 +0100

    Reformat day 8

--- lib/day_8.ex
+++ lib/day_8.ex
@@ -10,7 +10,8 @@ end
 defmodule AdventOfCode2023.Day8.Part1 do
   import AdventOfCode2023.Day8.Utility
 
-  def count_steps(map, key, [], directions_copy), do: count_steps(map, key, directions_copy, directions_copy)
+  def count_steps(map, key, [], directions_copy),
+    do: count_steps(map, key, directions_copy, directions_copy)
 
   def count_steps(map, key, [direction | tail], directions_copy) do
     next_key = map[key][direction]
@@ -27,17 +28,18 @@ defmodule AdventOfCode2023.Day8.Part1 do
   def solve do
     [dir | map_string] = get_file()
 
-    directions = dir |> String.to_charlist() |> Enum.map(&(if &1 == ?L, do: :left, else: :right))
+    directions = dir |> String.to_charlist() |> Enum.map(&if &1 == ?L, do: :left, else: :right)
 
-    map = map_string
-        |> Enum.map(&String.split(&1, [" = (", ", ", ")"], trim: true))
-        |> Enum.reduce(%{}, fn [key, left, right], acc ->
+    map =
+      map_string
+      |> Enum.map(&String.split(&1, [" = (", ", ", ")"], trim: true))
+      |> Enum.reduce(%{}, fn [key, left, right], acc ->
         Map.put(acc, key, left: left, right: right)
-        end)
+      end)
 
     map
     |> Enum.map(fn {key, _} ->
-      with [_,_,a] <- String.to_charlist(key) do
+      with [_, _, a] <- String.to_charlist(key) do
         if a == ?A do
           count_steps(map, key, directions, directions)
         else
@@ -47,7 +49,7 @@ defmodule AdventOfCode2023.Day8.Part1 do
     end)
     |> Enum.reduce(1, fn x, acc ->
       if x == 0 do
-	    acc
+        acc
       else
         div(acc * x, Integer.gcd(acc, x))
       end
